d88cb79f0c07c1e9c44a5dd617e71e98920da056
isurusiri <isuruanatomy@gmail.com>
2020-05-21 22:19:41 +0530

    implemented exporter's metrics exposing logic

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.12
 
 require (
 	github.com/Azure/azure-service-bus-go v0.6.0
+	github.com/marcinbudny/servicebus_exporter v0.0.0-20191115172311-8b2f1c3a14f2 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/namsral/flag v1.7.4-pre
-	github.com/prometheus/client_golang v0.9.2
+	github.com/prometheus/client_golang v1.6.0
 	github.com/sirupsen/logrus v1.6.0
 )
--- servicebus_exporter.go
+++ servicebus_exporter.go
@@ -1,5 +1,91 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/namsral/flag"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/sirupsen/logrus"
+
+	sb "github.com/isurusiri/azure_servicebus_exporter/client"
+	"github.com/isurusiri/azure_servicebus_exporter/collector"
+)
+
+var (
+	log = logrus.New()
+)
+
+type config struct {
+	timeout          time.Duration
+	port             uint
+	verbose          bool
+	connectionString string
+}
+
+func readAndValidateConfig() config {
+	var result config
+
+	flag.StringVar(&result.connectionString, "connection-string", "", "Azure ServiceBus connection string")
+	flag.UintVar(&result.port, "port", 9580, "Port to expose scraping endpoint on")
+	flag.DurationVar(&result.timeout, "timeout", time.Second*30, "Timeout for scrape")
+	flag.BoolVar(&result.verbose, "verbose", false, "Enable verbose logging")
+
+	flag.Parse()
+
+	if result.connectionString == "" {
+		log.Fatal("Azure ServiceBus connection string is not provided")
+	}
+
+	log.WithFields(logrus.Fields{
+		"port":    result.port,
+		"timeout": result.timeout,
+		"verbose": result.verbose,
+	}).Infof("Azure ServiceBus exporter has been configured")
+
+	return result
+}
+
+func configureRoutes() {
+	var landingPage = []byte(`<html>
+		<head><title>Azure ServiceBus exporter for Prometheus</title></head>
+		<body>
+		<h1>Azure ServiceBus exporter for Prometheus</h1>
+		<p><a href='/metrics'>Metrics</a></p>
+		</body>
+		</html>
+		`)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write(landingPage) // nolint: errcheck
+	})
+
+	http.Handle("/metrics", promhttp.Handler())
+}
+
+func setupLogger(config config) {
+	if config.verbose {
+		log.Level = logrus.DebugLevel
+	}
+}
+
+func startHTTPServer(config config) {
+	listenAddr := fmt.Sprintf(":%d", config.port)
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
+}
+
+func main() {
+	config := readAndValidateConfig()
+	setupLogger(config)
+
+	configureRoutes()
+
+	client := sb.New(config.connectionString, config.timeout)
+	coll := collector.New(client, log)
+	prometheus.MustRegister(coll)
+
+	startHTTPServer(config)
 }
